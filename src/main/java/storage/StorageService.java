1ac6ce48c605132e44699aad330f51fdac357ce1
Noor <nrandin@gmail.com>
2019-03-05 16:06:44 -0500

    changed from interface to class

---
+++ src/main/java/storage/StorageService.java
@@ -0,0 +1,16 @@
+package storage;
+
+import org.springframework.core.io.Resource;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
+public class StorageService {
+
+    private String path;
+
+    public void uploadFile(MultipartFile file){
+
+    }
+}
