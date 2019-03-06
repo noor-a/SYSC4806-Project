9495933b7469b7d46711c2f6b5e4e4a054a47d47
Noor <nrandin@gmail.com>
2019-03-06 08:58:12 -0500

    Test

--- src/main/java/models/SubmitterEntity.java
+++ src/main/java/models/SubmitterEntity.java
@@ -1,9 +1,11 @@
 package models;
 
 import javax.persistence.Entity;
+import java.util.*;
 
 @Entity
 public class SubmitterEntity extends UserEntity {
+    List<Article> submittedArticles;
 
     public SubmitterEntity(){
 
