6a2eb57c3c4b973c11aae43e3d5b34bb84225548
Noor <nrandin@gmail.com>
2019-03-06 02:05:00 -0500

    Added file upload function

--- src/main/java/controllers/AtricleController.java
+++ src/main/java/controllers/AtricleController.java
@@ -1,8 +1,46 @@
 package controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @Controller
 public class AtricleController {
+    private static String UPLOAD_FOLDER = "/uploadTemp/";
 
+    @GetMapping("/")
+    public String index(){
+        return "uploadForm";
+    }
+
+    @PostMapping("/uploadForm")
+    public String fileUpload(@RequestParam("file")MultipartFile file, RedirectAttributes redirectAttributes){
+        if(file.isEmpty()){
+            redirectAttributes.addFlashAttribute("message", "Empty File. Cannot upload");
+            return "redirect:/uploadStatus";
+        }
+
+        try{
+            byte[]bytes = file.getBytes();
+            Path path = Paths.get(UPLOAD_FOLDER + file.getOriginalFilename());
+            Files.write(path, bytes);
+            redirectAttributes.addFlashAttribute("message","Successfully uploaded '" + file.getOriginalFilename() + "'" );
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        return "redirect:/uploadStatus";
+    }
+
+    @GetMapping("/uploadStatus")
+    public String uploadStatus(){
+        return "uploadStatus";
+    }
 }
