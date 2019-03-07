88db95037ce6f13c2449f25f83f6fda5b7ef11b4
Manel <o.manel17@hotmail.com>
2019-03-06 20:12:13 -0500

    Created article repository

---
+++ src/main/java/repositoires/ArticleRepository.java
@@ -0,0 +1,12 @@
+package repositoires;
+
+import models.Article;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ArticleRepository {
+    List<Article> findById();
+    List<Article> findAll();
+}
