e1366cf3d074f36c9384e233224dc5a36cd5bcdd
Noor <nrandin@gmail.com>
2019-03-16 23:46:54 -0400

    Fixed file upload issue

--- src/main/java/application/WebSecurityConfig.java
+++ src/main/java/application/WebSecurityConfig.java
@@ -17,7 +17,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/home","/register", "/uploadForm").permitAll()
+                .antMatchers("/", "/home","/register", "/upload", "/uploadStatus").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
--- src/main/java/controllers/ArticleController.java
+++ src/main/java/controllers/ArticleController.java
@@ -1,18 +1,13 @@
 package controllers;
 
-import models.ArticleEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import repositories.ArticleRepository;
 
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -20,42 +15,34 @@ import java.nio.file.Paths;
 
 @Controller
 public class ArticleController {
-    private static String UPLOAD_FOLDER = "/uploadTemp/";
+    public static String UploadDirectory = System.getProperty("user.dir")+"/uploads";
 
     @Autowired
     ArticleRepository articleRepository;
 
-
-    @PostMapping("/uploadForm")
-    public String fileUpload(@RequestParam("file")MultipartFile file, RedirectAttributes redirectAttributes, Model model){
-        if(file.isEmpty()){
-            redirectAttributes.addFlashAttribute("message", "Empty File. Cannot upload");
-            return "redirect:/uploadStatus";
-        }
-
-        try{
-            byte[]bytes = file.getBytes();
-            Path path = Paths.get(UPLOAD_FOLDER + file.getOriginalFilename());
-
-            //Converts MultiPartFile into File
-            File f = new File(file.getOriginalFilename());
-            f.createNewFile();
-            FileOutputStream fos = new FileOutputStream(f);
-            fos.write(file.getBytes());
-            fos.close();
-
-            Files.write(path, bytes);
-            model.addAttribute("article", new ArticleEntity(f));
-            articleRepository.save(new ArticleEntity(f));
-            redirectAttributes.addFlashAttribute("message","Successfully uploaded '" + file.getOriginalFilename() + "'" );
-        }catch (IOException e){
-            e.printStackTrace();
-        }
-        return "redirect:/uploadStatus";
+    @RequestMapping("/upload")
+    public String uploadPage(Model model){
+        return "uploadView";
     }
 
-    @GetMapping("/uploadStatus")
-    public String uploadStatus(){
+    @RequestMapping("/upload")
+    public String fileUpload(Model model, @RequestParam("files") MultipartFile[] files){
+        StringBuilder fileNames = new StringBuilder();
+        for(MultipartFile file: files){
+            Path fileNameAndPath = Paths.get(UploadDirectory, file.getOriginalFilename());
+            fileNames.append(file.getOriginalFilename());
+            try {
+                Files.write(fileNameAndPath, file.getBytes());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        model.addAttribute("msg", "Successfully Uploaded files "+ fileNames.toString());
         return "uploadStatus";
     }
+
+    /*@GetMapping("/uploadStatus")
+    public String uploadStatus(){
+        return "uploadStatus";
+    }*/
 }
