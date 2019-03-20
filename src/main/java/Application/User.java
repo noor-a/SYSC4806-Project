8fb156e7b309631f69061ec1d1ec0d15951b4780
Kyle Smith <ksmith@renrns.com>
2019-03-20 00:44:08 -0400

    Updated the user class to include type

--- src/main/java/Application/User.java
+++ src/main/java/Application/User.java
@@ -19,6 +19,8 @@ public class User {
     private String lastName;
 
     private String email;
+    
+    private String type;//This should either ve "editor", "reviewer" or "submitter"
 
     public long getId() {
         return id;
@@ -66,6 +68,14 @@ public class User {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+    
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
     }
 
 }
