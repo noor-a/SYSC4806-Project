cde02f1930be21ee3594f416b8a55835a6bc709a
Noor <nrandin@gmail.com>
2019-03-19 23:47:15 -0400

    Added files to other branch

---
+++ src/main/java/Application/ArticleController.java
@@ -0,0 +1,57 @@
+package Application;
+
+import models.Article;
+import models.Status;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import repositories.ArticleRepository;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class ArticleController {
+    public static String UPLOAD_DIR = System.getProperty("user.dir")+"/uploads";
+
+    @Autowired
+    ArticleRepository articleRepository;
+
+    @RequestMapping(value="/upload", method = RequestMethod.GET)
+    public String uploadHandler(Model model){
+
+        return "uploadForm";
+    }
+
+    @RequestMapping(value="/upload", method = RequestMethod.POST)
+    public String submit(@RequestParam("file") MultipartFile file, Model model){
+        model.addAttribute("file", file);
+
+        //Creates a new article with the file and saves it to the repository
+        Article a = new Article();
+        a.setUploadFile(file);
+        a.setStatus(Status.SUMBITRED);
+        articleRepository.save(a);
+        return "uploadView";
+    }
+
+    @GetMapping("/uploadView")
+    public String getUploadedFiles(){
+        return "uploadView";
+    }
+
+    //Gets Returns all articles that have been uploaaded
+    public List<Article> getAllArticles(){
+        return articleRepository.findAll();
+    }
+}
---
+++ src/main/java/Application/Status.java
@@ -0,0 +1,9 @@
+package application;
+
+public enum Status {
+
+    SUMBITRED,
+    IN_REVIEW,
+    ACCEPTED,
+    REJECTED
+}
