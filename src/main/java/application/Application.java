738310efed497909a74fb7a0c1d0281842cec092
Noor <nrandin@gmail.com>
2019-03-19 22:04:57 -0400

    updated

--- src/main/java/application/Application.java
+++ src/main/java/application/Application.java
@@ -1,19 +1,13 @@
 package application;
 
-import controllers.ArticleController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-
-import java.io.File;
 
 
 @SpringBootApplication
-//@ComponentScan({"application", "controllers"})
 public class Application {
 
     public static void main(String[] args) {
-        new File(ArticleController.UploadDirectory).mkdir();
         SpringApplication.run(Application.class, args);
     }
 
