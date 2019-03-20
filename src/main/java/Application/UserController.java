23cfe8b1c1adac1c159c58d3f7194941f1bd02e9
darshgrewal <grewaldarshpreet@gmail.com>
2019-03-19 21:34:25 -0400

    Added Authentication Controller

--- src/main/java/Application/UserController.java
+++ src/main/java/Application/UserController.java
@@ -11,34 +11,29 @@ public class UserController {
     @Autowired
     UserRepository repository;
 
-    @GetMapping("/register")
-    public String index(Model model) {
-        return "register";
-    }
-
-    @PostMapping("/register")
-    public String addUser(@ModelAttribute User user, Model model) {
-
-        model.addAttribute("newUser", new User());
-        return "register";
-    }
-
     //creates a user object
-    @PostMapping(value="/createUser")
-    public String createUser(@RequestParam("firstName") String firstName,
-                                @RequestParam("lastName") String lastName,
-                                @RequestParam("email") String email,
-                                @RequestParam("username") String username,
-                                @RequestParam("password") String password){
-        User u = new User();
-        u.setFirstName(firstName);
-        u.setLastName(lastName);
-        u.setEmail(email);
-        u.setUsername(username);
-        u.setPassword(password);
-        repository.save(u);
+    @RequestMapping(value = "/createUser", method = RequestMethod.GET)
+    public String registerEditor(@RequestParam("username") String username,
+                                 @RequestParam("password") String password,
+                                 @RequestParam("firstName") String firstName,
+                                 @RequestParam("lastName") String lastName,
+                                 @RequestParam("email") String email) {
+        User e = new User();
+        e.setUsername(username);
+        e.setPassword(password);
+        e.setFirstName(firstName);
+        e.setLastName(lastName);
+        e.setUsername(email);
+        repository.save(e);
+        System.out.println("hello");
         return "home";
     }
+
+    @RequestMapping(value="/createUsers",method=RequestMethod.GET)
+    public String createUsers(){
+        return "register";
+    }
+
 
 
 }
