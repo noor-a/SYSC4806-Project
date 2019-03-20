e336102824454150ebbd8af8e56317ade4c758d6
Daman <damanjitbhangoo@gmail.com>
2019-03-19 22:35:03 -0400

    Added login features and authentication.

---
+++ src/main/java/Application/AuthenticationController.java
@@ -0,0 +1,61 @@
+package Application;
+
+import java.util.List;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AuthenticationController {
+
+    private UserRepository repository;
+    private int nextSession = 0;
+
+    @Autowired
+    public void setRepository(UserRepository repository){
+        this.repository = repository;
+    }
+
+    @RequestMapping(value="/login",method=RequestMethod.GET)
+    public String verifyUser(@RequestParam("username") String username,
+                             @RequestParam("password") String password,
+                             Model model,
+                             HttpServletResponse response){
+
+        List<User> users = repository.findByUsername(username);
+        if(users.isEmpty()){
+            model.addAttribute("message", "User not found");
+            return "loginFailed";
+        }
+        User u = users.get(0);
+        if(u.getPassword().equals(password)){
+            repository.save(u);
+            return "home";
+        } else {
+            model.addAttribute("message", "Password incorrect");
+            return "loginFailed";
+        }
+    }
+
+    @RequestMapping(value="/logout",method=RequestMethod.GET)
+    public String logout(Model model,
+                         HttpServletResponse response){
+
+        response.addCookie(new Cookie("sessionId",null));
+        return "login";
+    }
+
+    @RequestMapping(value="/logins",method=RequestMethod.GET)
+    public String logins(){
+        return "login";
+    }
+
+
+}
